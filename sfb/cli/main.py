7175a07b51039d8b8fde7e296cdc6b6296ff7afd
tosh223 <rev.to12@gmail.com>
2021-01-23 23:38:31 +0900

    change entrypoint

--- sfb/cli/main.py
+++ sfb/cli/main.py
@@ -2,15 +2,28 @@
 Entry point for the CLI
 """
 import sys
+import argparse
+
 from sfb.bq import Estimator
 
 def cli():
-    if len(sys.argv) == 2:
-        estimator = Estimator(timeout=None)
-        response = estimator.check(sys.argv[1])
-        print(response)
-    else:
-        print('Please set a argument: sql_file_path')
+    __parser = argparse.ArgumentParser()
+    __parser.add_argument(
+        "sql_file_path", 
+        help="sql file path",
+        type=str,
+    )
+    __parser.add_argument(
+        "-t", "--timeout", 
+        help="request timeout seconds",
+        type=float,
+    )
+    __args = __parser.parse_args()
+
+    print(f"target_file: {__args.sql_file_path}")
+    __estimator = Estimator(timeout=__args.timeout)
+    __response = __estimator.check(__args.sql_file_path)
+    print(__response)
 
 if __name__ == '__main__':
     cli()
