7c53713953c6b60ffaf1c97a2a169b20242ce7aa
tosh223 <rev.to12@gmail.com>
2021-02-20 12:03:31 +0900

    fix-tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,23 +15,18 @@ def estimator():
     estimator = BigQueryEstimator()
     return estimator
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def check_file_success(estimator):
     return estimator.check_file(sql_file_success)
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def check_file_failure_notfound(estimator):
     return estimator.check_file(sql_file_failure_nf)
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def check_file_failure_badrequest_01(estimator):
     return estimator.check_file(sql_file_failure_br_01)
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def check_file_failure_badrequest_02(estimator):
     return estimator.check_file(sql_file_failure_br_02)
-
-@pytest.fixture(scope="session")
-def check_file_failure_timeout():
-    estimator_timeout = BigQueryEstimator(timeout=0.1)
-    return estimator_timeout.check_file(sql_file_success)
